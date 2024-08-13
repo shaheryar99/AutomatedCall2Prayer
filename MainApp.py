6b521504a85ab08a05b2ab7320483ad6094227d0
Shaheryar Syed <63892413+shaheryar99@users.noreply.github.com>
2024-08-12 23:57:42 -0600

    Add files via upload
    .wav file has been modified to fit the codec of the RasberryPi & pygame. 
    
    Can be downloaded here:
    
    https://drive.google.com/file/d/1oTy_9PT9ewVlLxSm258GEqtl62o1l3iC/view?usp=sharing

---
+++ MainApp.py
@@ -0,0 +1,100 @@
+from datetime import date, timedelta, datetime
+import json
+import os
+import requests
+import time
+#import simpleaudio as sa
+import pygame
+
+# Midnight + 10 minutes to account for date change
+midnight = datetime.now().replace(hour=00, minute=10).strftime("%H:%M")
+
+dummyTime = datetime.now().replace(hour=3, minute=51).strftime("%H:%M")
+dummyTime1 = datetime.now().replace(hour=3, minute=57).strftime("%H:%M")
+dummyTime2 = datetime.now().replace(hour=1, minute=5).strftime("%H:%M")
+
+reqParameters = {'latitude' : '51.049999', 'longitude' : '-114.066666', 'method' : '0'}
+
+# Populate timings
+def populateTimings():
+        timeNow = datetime.now().strftime("%H:%M")
+        dateToday = date.today()
+        year = dateToday.year
+        month = dateToday.month
+        day = dateToday.day
+
+        yearstr = str(year)
+        monthstr = str(month)
+        daystr = str(day)
+
+        datestring = daystr + "-" + monthstr + "-" + yearstr
+
+        getData = requests.get('https://api.aladhan.com/v1/timings/' + datestring, params=reqParameters)
+        dataParsed = getData.json()
+        return dataParsed
+
+dataParsed = populateTimings()
+
+#azan = sa.WaveObject.from_wave_file("Azan.wav")
+
+pygame.init()
+pygame.mixer.init()
+sound = pygame.mixer.Sound("/etc/Azan.wav")
+
+while True:
+    # Current Time
+    timeNow = datetime.now().strftime("%H:%M")
+
+    timings = dataParsed['data']['timings']
+
+    fajr = timings['Fajr']
+    zuhr = timings['Dhuhr']
+    magrib = timings['Maghrib']
+
+    fajrtime = datetime.strptime(fajr, '%H:%M')
+    addFive = timedelta(minutes=6)
+    fajrtime = fajrtime + addFive
+
+    zuhrtime = datetime.strptime(zuhr, '%H:%M')
+
+    magribtime = datetime.strptime(magrib, '%H:%M')
+    minusTwo = timedelta(minutes=1)
+    magribtime = magribtime - minusTwo
+
+    fajrTimeString = fajrtime.strftime("%H:%M")
+    zuhrTimeString = zuhrtime.strftime("%H:%M")
+    magribTimeString = magribtime.strftime("%H:%M")
+
+    if (timeNow == fajrTimeString):
+        print("Playing Azan for Fajr")
+        #play_azan = azan.play()
+        #play_azan.wait_done()
+        sound.play()
+
+    elif (timeNow == zuhrTimeString):
+        print("Playing Azan for Zuhr")
+        #play_azan = azan.play()
+        #play_azan.wait_done()
+        sound.play()
+
+    elif (timeNow == magribTimeString):
+        print("Playing Azan for Magrib")
+        #play_azan = azan.play()
+        #play_azan.wait_done()
+        sound.play()
+
+    elif (timeNow == midnight):
+        dataParsed = populateTimings()
+
+    #elif (timeNow == dummyTime):
+        #play_azan = azan.play()
+        #play_azan.wait_done()
+         #sound.play()
+
+    #elif (timeNow == dummyTime1):
+        #play_azan = azan.play()
+        #play_azan.wait_done()
+        #sound.play()
+
+    time.sleep(60)
+
